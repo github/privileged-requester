b0b8a26a382521358fdb80dd01c9012412ea2c1e
Dan Hoerst <danhoerst@github.com>
2022-10-04 13:02:26 -0400

    Update Action functionality

--- index.js
+++ index.js
@@ -1,18 +1,32 @@
 const github = require('@actions/github');
 const core = require('@actions/core');
+const yaml = require('js-yaml');
 
 async function run() {
   const myToken = core.getInput('myToken');
-
   const octokit = github.getOctokit(myToken)
 
   const { data: configContent } = await octokit.rest.repos.getContent({
     owner: core.getInput('owner'),
     repo: core.getInput('repo'),
     path: core.getInput('path'),
+    mediaType: { format: "raw" }
   });
 
-  console.log(configContent);
+  const prCreator = core.getInput('prCreator').toLowerCase();
+  const contents = yaml.load(configContent);
+  const requesters = contents["requesters"];
+
+  for (const [privileged_requester_username, privileged_requester_config] of Object.entries(requesters)) {
+    // console.log(privileged_requester_username);
+    // If privileged_requester_username is not the creator of the PR, move on
+    // If privileged_requester_username is the creator of the PR, check the remaining config
+    if (prCreator === privileged_requester_username) {
+      console.log("Privileged requester found. Checking PR criteria against the privileged requester configuration.");
+    } else {
+      console.log("Not privileged requester!");
+    }
+  }
 }
 
 run();
