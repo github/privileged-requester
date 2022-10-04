c3ca40f8a02a28da73c8d246265ca8275c7403e1
Dan Hoerst <danhoerst@github.com>
2022-10-04 11:53:17 -0400

    Update

--- dist/index.js
+++ dist/index.js
@@ -9227,27 +9227,17 @@ const github = __nccwpck_require__(5438);
 const core = __nccwpck_require__(2186);
 
 async function run() {
-  // This should be a token with access to your repository scoped in as a secret.
-  // The YML workflow will need to set myToken with the GitHub Secret Token
-  // myToken: ${{ secrets.GITHUB_TOKEN }}
-  // https://help.github.com/en/actions/automating-your-workflow-with-github-actions/authenticating-with-the-github_token#about-the-github_token-secret
   const myToken = core.getInput('myToken');
 
   const octokit = github.getOctokit(myToken)
 
-  // You can also pass in additional options as a second parameter to getOctokit
-  // const octokit = github.getOctokit(myToken, {userAgent: "MyActionVersion1"});
-
-  const { data: pullRequest } = await octokit.pulls.get({
-    owner: 'octokit',
-    repo: 'rest.js',
-    pull_number: 123,
-    mediaType: {
-      format: 'diff'
-    }
+  const { data: configContent } = await octokit.rest.repos.getContent({
+    owner: core.getInput('owner'),
+    repo: core.getInput('repo'),
+    path: core.getInput('path'),
   });
 
-  console.log(pullRequest);
+  console.log(configContent);
 }
 
 run();
