ebe52ee11960a4da92e3734e52483927252d18c8
Dan Hoerst <danhoerst@github.com>
2022-10-04 15:02:44 -0400

    Working approval functionality

--- index.js
+++ index.js
@@ -22,58 +22,57 @@ async function run() {
     // console.log(privileged_requester_username);
     // If privileged_requester_username is not the creator of the PR, move on
     // If privileged_requester_username is the creator of the PR, check the remaining config
-    if (prCreator === privileged_requester_username) {
-      console.log("Privileged requester found. Checking PR criteria against the privileged requester configuration.");
-      // Check all commits of the PR to verify that they are all from the privileged requester, otherwise Fail? or at least, return
-      const { data: prCommits } = await octokit.rest.pulls.listCommits({
-        owner: github.context.repo.owner,
-        repo: github.context.repo.repo,
-        pull_number: prNumber
-      });
-      for (const [, commit] of Object.entries(prCommits)) {
-        let commitAuthor = commit.author.login.toLowerCase();
-        let commitCommitter = commit.committer.login.toLowerCase();
-        // Each commit returns a "committer" and an "author". In all cases I've tested, they are the same.
-        // However, since they could be different, lets check both
-        if (commitAuthor !== privileged_requester_username || commitCommitter !== privileged_requester_username) {
-          console.log("Unexpected commit found! I will not proceed with the privileged reviewer process.");
-          return 0;
-        }
-      }
+    console.log(`PR creator is ${prCreator}. Testing against ${privileged_requester_username}`)
+    if (prCreator !== privileged_requester_username) {
+      continue;
+    }
 
-      // Check labels of the PR to make sure that they match the privileged_requester_config
-      const { data: prLabels } = await octokit.rest.issues.listLabelsOnIssue({
-        owner: github.context.repo.owner,
-        repo: github.context.repo.repo,
-        issue_number: prNumber
-      });
+    console.log(`Privileged requester ${privileged_requester_username} found. Checking PR criteria against the privileged requester configuration.`);
+    // Check all commits of the PR to verify that they are all from the privileged requester, otherwise Fail? or at least, return
+    const { data: prCommits } = await octokit.rest.pulls.listCommits({
+      owner: github.context.repo.owner,
+      repo: github.context.repo.repo,
+      pull_number: prNumber
+    });
+    for (const [, commit] of Object.entries(prCommits)) {
+      let commitAuthor = commit.author.login.toLowerCase();
 
-      const prLabelArray = [];
-
-      for (const [, prLabel] of Object.entries(prLabels)) {
-        let prLabelName = prLabel.name;
-        prLabelArray.push(prLabelName);
-      }
-
-      let difference = prLabelArray.filter(x => !privileged_requester_config.labels.includes(x));
-      if (difference.length === 0) {
-        console.log("Invalid label found. I will not proceed with the privileged reviewer process.");
+      if (commitAuthor !== privileged_requester_username) {
+        console.log(`Unexpected commit author found by ${commitAuthor}! Commits should be authored by ${privileged_requester_username} I will not proceed with the privileged reviewer process.`);
         return 0;
       }
+    }
 
-      // If we've gotten this far, the commits are all from the privileged requestor and the labels are correct
-      // We can now approve the PR
-      console.log("Approving the PR for a privileged reviewer.")
-      await octokit.rest.pulls.createReview({
-        owner: github.context.repo.owner,
-        repo: github.context.repo.repo,
-        pull_number: prNumber,
-        event: "APPROVE"
-      });
-      console.log("PR approved, all set!")
-    } else {
+    // Check labels of the PR to make sure that they match the privileged_requester_config
+    const { data: prLabels } = await octokit.rest.issues.listLabelsOnIssue({
+      owner: github.context.repo.owner,
+      repo: github.context.repo.repo,
+      issue_number: prNumber
+    });
+
+    const prLabelArray = [];
+
+    for (const [, prLabel] of Object.entries(prLabels)) {
+      let prLabelName = prLabel.name;
+      prLabelArray.push(prLabelName);
+    }
+
+    let differences = prLabelArray.filter(x => !privileged_requester_config.labels.includes(x));
+    if (differences.length !== 0) {
+      console.log(`Invalid label(s) found: ${differences}. I will not proceed with the privileged reviewer process.`);
       return 0;
     }
+
+    // If we've gotten this far, the commits are all from the privileged requestor and the labels are correct
+    // We can now approve the PR
+    console.log("Approving the PR for a privileged reviewer.")
+    await octokit.rest.pulls.createReview({
+      owner: github.context.repo.owner,
+      repo: github.context.repo.repo,
+      pull_number: prNumber,
+      event: "APPROVE"
+    });
+    console.log("PR approved, all set!")
   }
 }
 
